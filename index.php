7ea6cfdf97c30e4e84079583d5ecbe61b186ceb2
Abhi <abhihesta@gmail.com>
2022-08-28 10:23:38 +0530

    add php mailer file

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+$mail = new PHPMailer(true);
+
+try {
+ 
+                  
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.gmail.com';                     
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = 'abhishekkumarnic637@gmail.com';                    
+    $mail->Password   = 'vxewsnqgmenmmoxi';                             
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
+    $mail->Port       = 465;                                    
+
+  
+    $mail->setFrom('abhishek@gmail.com', 'Subscribe kr lene is channel ko');
+
+    $mail->addAddress('');             
+
+
+    $mail->isHTML(true);                                 
+    $mail->Subject = 'subscribe to this channel for more video update'.time();
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
